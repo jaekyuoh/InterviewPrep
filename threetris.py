bfe3b90dbc49535da8e3c2ba9cf5e507e1d9f548
unknown <jaekyuoh@gmail.com>
2019-07-03 21:07:34 +0900

    adding threetris.py

---
+++ threetris.py
@@ -0,0 +1,11 @@
+
+
+def solution(block, board):
+    answer = 0 
+    return answer
+
+if __name__ == "__main__":
+    block = 0
+    board = [[1,0,0,0], [1,0,0,1],[1,1,0,1],[1,1,0,1]]
+    answer = 2
+    print(solution(block, board))
